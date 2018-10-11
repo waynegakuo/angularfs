91e2abc47ddcb931fa32bb7ddcfa9bac9a322c6b
Wayne Gakuo <waynejotham@gmail.com>
2018-10-11 22:11:16 +0000

    Added the Firebase connection block into environment.ts file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyAFCpk5QHldRyrVScJ9beIrpnkkiQAhlO0",
+    authDomain: "fs1prod-37953.firebaseapp.com",
+    databaseURL: "https://fs1prod-37953.firebaseio.com",
+    projectId: "fs1prod-37953",
+    storageBucket: "fs1prod-37953.appspot.com",
+    messagingSenderId: "531396950714"
+  }
 };
 
 /*
