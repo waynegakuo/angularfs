1e9dbde5b93ebb40b034376d2ecbf699d5bf11b5
Wayne Gakuo <waynejotham@gmail.com>
2018-10-11 23:13:50 +0000

    Addd collection to class ItemService and contuctor, created an exportable interface through creating models

---
+++ src/app/models/item.ts
@@ -0,0 +1,5 @@
+interface Item{
+  id?: string; //means optional
+  title?: string;
+  description?: string;
+}
--- src/app/services/item.service.ts
+++ src/app/services/item.service.ts
@@ -1,10 +1,16 @@
 import { Injectable } from '@angular/core';
 
+import {AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument}
+from 'angularfire2/firestore';
+
+
 @Injectable({
   providedIn: 'root'
 })
 export class ItemService {
-  
+  itemsCollection: AngularFirestoreCollection <Item>;
 
-  constructor() { }
+  constructor(public afs: AngularFirestore) {
+
+  }
 }
