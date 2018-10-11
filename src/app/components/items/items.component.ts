a69e882b57a7cb6d9588010df6109aa8d0e224b9
Wayne Gakuo <waynejotham@gmail.com>
2018-10-11 23:26:47 +0000

    We are now able to fetch from Firebase

--- src/app/components/items/items.component.ts
+++ src/app/components/items/items.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ItemService} from '../../services/item.service'
 
 @Component({
   selector: 'app-items',
@@ -7,10 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ItemsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private itemService: ItemService) {}
 
   ngOnInit() {
-
+    this.itemService.getItems().subscribe(items => {
+      console.log(items);
+    })
   }
 
 }
