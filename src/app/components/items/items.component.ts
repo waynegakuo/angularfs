2488c1a391fb22fd6b25afb52b265c332f4e49c7
Wayne Gakuo <waynejotham@gmail.com>
2018-10-12 00:12:10 +0000

    Can noget data from Firebase and display it on themain html

--- src/app/components/items/items.component.ts
+++ src/app/components/items/items.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {ItemService} from '../../services/item.service'
+import {ItemService} from '../../services/item.service';
+import {Item} from '../../models/item'; //we need to import our interface from model
+
 
 @Component({
   selector: 'app-items',
@@ -7,12 +9,14 @@ import {ItemService} from '../../services/item.service'
   styleUrls: ['./items.component.css']
 })
 export class ItemsComponent implements OnInit {
+  items: Item[]; //coming from our interface
 
   constructor(private itemService: ItemService) {}
 
   ngOnInit() {
-    this.itemService.getItems().subscribe(items => {
-      console.log(items);
+    this.itemService.getItems().subscribe(subItems => {
+      //console.log(subItems);
+      this.items=subItems; //getting items through Observable and setting them to items
     })
   }
 
