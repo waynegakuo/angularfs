61d0c3c5f3fa5c073ba366556b8b1fc8adf74f86
Wayne Gakuo <waynejotham@gmail.com>
2018-10-17 03:59:41 +0000

    Preparation for deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyAFCpk5QHldRyrVScJ9beIrpnkkiQAhlO0",
+    authDomain: "fs1prod-37953.firebaseapp.com",
+    databaseURL: "https://fs1prod-37953.firebaseio.com",
+    projectId: "fs1prod-37953",
+    storageBucket: "fs1prod-37953.appspot.com",
+    messagingSenderId: "531396950714"
+  }
 };
